32b188f60a68e8840ad57638192daecb0b55a2d7
DanielGrajales-U <dani22.24gra@gmail.com>
2025-09-29 14:48:56 -0500

    clean code

--- First-certamen/Challenge1/index.js
+++ First-certamen/Challenge1/index.js
@@ -1,45 +1,4 @@
-const merge = (left, right) => {
-  let resultArray = [],
-    leftIndex = 0,
-    rightIndex = 0
-
-  while (leftIndex < left.length && rightIndex < right.length) {
-    if (left[leftIndex] < right[rightIndex]) {
-      resultArray.push(left[leftIndex])
-      leftIndex++
-    } else {
-      resultArray.push(right[rightIndex])
-      rightIndex++
-    }
-  }
-
-  return resultArray.concat(left.slice(leftIndex)).concat(right.slice(rightIndex))
-}
-
-const mergeSort = (array) => {
-  if (array.length === 1) {
-    return array
-  }
-
-  const middle = Math.floor(array.length / 2)
-  const left = array.slice(0, middle)
-  const right = array.slice(middle)
-
-  return merge(mergeSort(left), mergeSort(right))
-}
-
 export function validSequences(notas) {
-  if (notas.length < 2) return 0
-
-  const sortArray = mergeSort(notas)
-
-  const setArray = new Set(sortArray)
-  if (setArray.size !== sortArray.length) return 0
-
-  const min = sortArray[0]
-  const max = sortArray[sortArray.length - 1]
-  if (max - min + 1 === sortArray.length) return 1
-
-  return 0
+  //Implement the solution  
 }
 
